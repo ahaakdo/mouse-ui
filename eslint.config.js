3c249e3b5d32a41387378db8b99afadf9d15b2eb
ahaakdo <1197235629@qq.com>
2024-06-27 16:21:04 +0800

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // 其他配置项...
+    ignores: [
+        'node_modules/', // 忽略 node_modules 目录下的所有文件
+        'dist/',         // 忽略 dist 目录下的所有文件
+        '.*rc.js',
+        '*.config.js',
+        '*.css',
+        '*.pcss',
+        '*.scss',
+        '*.json'
+    ],
+};
